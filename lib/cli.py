47d115b380df24679fe2bf8b797c4880e641ef31
Ricardo Guerra <RikkuG491@gmail.com>
2024-10-21 16:28:54 -0400

    Added helper methods to interact with Flight model methods

--- lib/cli.py
+++ lib/cli.py
@@ -2,27 +2,42 @@
 
 from helpers import (
     exit_program,
-    helper_1
+    create_flight,
+    delete_flight,
+    retrieve_all_flights,
+    retrieve_flight_by_id,
+    retrieve_flight_bookings
 )
-
 
 def main():
     while True:
         menu()
         choice = input("> ")
+        print(choice)
         if choice == "0":
             exit_program()
         elif choice == "1":
-            helper_1()
+            create_flight()
+        elif choice == "2":
+            delete_flight()
+        elif choice == "3":
+            retrieve_all_flights()
+        elif choice == "4":
+            retrieve_flight_by_id()
+        elif choice == "5":
+            retrieve_flight_bookings()
         else:
             print("Invalid choice")
-
+        input("\nPress 'return' to continue... \n")
 
 def menu():
     print("Please select an option:")
     print("0. Exit the program")
-    print("1. Some useful function")
-
+    print("1. Create new flight")
+    print("2. Delete a flight")
+    print("3. Retrieve all flights")
+    print("4. Retrieve flight by id")
+    print("5. Retrieve bookings for a flight")
 
 if __name__ == "__main__":
     main()
--- lib/helpers.py
+++ lib/helpers.py
@@ -1,8 +1,65 @@
 # lib/helpers.py
 
-def helper_1():
-    print("Performing useful function#1.")
+from models.flight import Flight
+from models.booking import Booking
 
+def create_flight():
+    airline = input("Enter the airline for the flight > ")
+    origin = input("Enter the origin for the flight > ")
+    destination = input("Enter the destination for the flight > ")
+    price = input("Enter the price for the flight > ")
+
+    try:
+        new_flight = Flight.create(airline, origin, destination, float(price))
+        print("New flight successfully added! Here are the details for the new flight:")
+        print(new_flight)
+    except:
+        print("Error: Unable to add new flight due to invalid information entered!")
+
+def delete_flight():
+    id = input("Enter the id for the flight that you want to delete > ")
+    flight = Flight.find_by_id(id)
+    
+    if flight:
+        flight.delete()
+        print(f"Flight # {id} was successfully deleted!")
+    else:
+        print(f"Unable to delete Flight # {id} since this flight does not exist!")
+
+def retrieve_all_flights():
+    flights = Flight.get_all()
+
+    if len(flights) > 0:
+        print("Here is the information for all of the flights:")
+        for flight in flights:
+            print(flight)
+    else:
+        print("There are no flights available!")
+
+def retrieve_flight_by_id():
+    id = input("Enter the id for the flight that you want to retrieve > ")
+    flight = Flight.find_by_id(id)
+
+    if flight:
+        print(f"Here is the information for Flight # {id}:")
+        print(flight)
+    else:
+        print(f"Error: Flight # {id} Not Found!")
+
+def retrieve_flight_bookings():
+    id = input("Enter the id for the flight that you want to retrieve bookings for > ")
+    flight = Flight.find_by_id(id)
+
+    if flight:
+        flight_bookings = flight.bookings()
+        if len(flight_bookings) > 0:
+            print(f"Here is the information for the bookings for Flight # {id}:")
+            for booking in flight_bookings:
+                print(booking)
+        else:
+            print(f"Flight # {id} does not have any bookings!")
+    else:
+        print(f"Error: Flight # {id} Not Found!")
 
 def exit_program():
     print("Goodbye!")
